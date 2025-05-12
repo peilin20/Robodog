e0f5f99b9b8671271496323867e71e7633a5adb7
yixiaxie <yixiaxie@usc.edu>
2025-05-11 17:17:22 -0700

    MENU

---
+++ Unity/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static PauseMenu Instance;
+
+    public GameObject menuUI; // 拖入菜单Canvas
+    private bool isPaused = false;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    // Only called by the Pause button
+    public void OpenMenu()
+    {
+        if (isPaused) return;
+        menuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void Resume()
+    {
+        menuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void RestartLevel()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("TutorialFinal");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit Game");
+    }
+}
